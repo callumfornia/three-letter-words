627c8b20d6fefea70a44d13666e041ece6e192d7
callumfornia <cal.mcneily@gmail.com>
2020-11-27 13:02:07 -0800

    inital commit

---
+++ three-letter-words.py
@@ -0,0 +1,24 @@
+### three-letter-words.py ###
+
+
+words = []
+game = "play"
+
+while game == "play":
+	
+	new = input("Enter a new 3 letter word: ")
+	if len(new) != 3:
+		print("That's not a 3-letter word")
+
+	else:
+			if new in words:
+				game = "over"
+				print("You alread said that word. Game over.")
+				print("You know " + str(len(words)) + " 3-letter words.")
+				print(words)
+			
+			else:
+				print("Nice one.")
+				words.append(new)
+
+
